e3dd685c3153d75dfc36478290e7d9fee98d7d4d
DipeshParmar18 <vrajdigitalmarketing18@gmail.com>
2026-04-25 16:52:03 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: fileURLToPath(new URL('./index.html', import.meta.url)),
+    },
+  },
 })
